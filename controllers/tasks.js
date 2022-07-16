5da27b33a8bd11c66771233ce47c7f21301a45bb
tomm66 <abzlove102@gmail.com>
2022-07-16 21:13:03 +1000

    Added update and delete task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -32,15 +32,41 @@ const getSingleTask= async (req, res) => {
         }
 
 }
-const updateTask=(req, res) => {
-    res.send("Update the task")
+const updateTask= async (req, res) => {
+     try {
+        const updateTask = await Task.findOneAndUpdate(
+            {_id: req.params.id},
+            req.body,
+            {
+             new : true,
+            }
+            )
+
+        if(!updateTask){
+            return res.status(404).json(`_id:${req.params.id} doesn't exist`)
+
+        }
+        res.status(200).json(updateTask)
+        } catch (err) {
+            res.status(500).json(err)
+        }
 
 }
 
-const deleteTask=(req, res) => {
-    res.send("Delete the task")
+const deleteTask= async (req, res) => {
+    try {
+        const deleteTask = await Task.findOneAndDelete(
+            {_id: req.params.id})
 
-}
+        if(!deleteTask){
+            return res.status(404).json(`_id:${req.params.id} doesn't exist`)
+
+        }
+        res.status(200).json(deleteTask)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    }
 
 module.exports ={
     getAllTasks,
